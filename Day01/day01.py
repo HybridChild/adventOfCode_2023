ee54bb9fef30a628d7c1701c9aa589278523e2be
Esben Dueholm Nørgaard <esben.dueholmnorgaard@musictribe.com>
2023-12-01 07:38:13 +0100

    Day1 - Part1 Solved.

--- Day01/day01.py
+++ Day01/day01.py
@@ -7,3 +7,26 @@ absFilePath = os.path.join(scriptDir, relFilePath)
 
 with open(absFilePath) as inputFile:
     inputStr = inputFile.read()
+
+inputList = inputStr.splitlines()
+
+# Part 1
+calibrationValueList = []
+
+for line in inputList:
+    digit1 = None
+    digit2 = None
+    scanRightCount = 0
+    scanLeftCount = len(line) - 1
+    while digit1 is None or digit2 is None:
+        if line[scanRightCount].isdigit() and digit1 is None:
+            digit1 = line[scanRightCount]
+        if line[scanLeftCount].isdigit() and digit2 is None:
+            digit2 = line[scanLeftCount]
+        scanRightCount += 1
+        scanLeftCount -= 1
+
+    calibrationValue = int(digit1 + digit2)
+    calibrationValueList.append(calibrationValue)
+
+print(sum(calibrationValueList))

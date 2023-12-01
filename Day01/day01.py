a8b625a73fb4e38c523f06717643bb144d862f7e
Esben Dueholm Nørgaard <esben.dueholmnorgaard@musictribe.com>
2023-12-01 07:14:19 +0100

    Initial commit.

---
+++ Day01/day01.py
@@ -0,0 +1,9 @@
+import os
+
+# --- Start here ---
+scriptDir = os.path.dirname(__file__)
+relFilePath = 'input.txt'
+absFilePath = os.path.join(scriptDir, relFilePath)
+
+with open(absFilePath) as inputFile:
+    inputStr = inputFile.read()
---
+++ README.md
@@ -0,0 +1,2 @@
+My (attempts at) solutions for AdventOfCode 2023.
+

aa35488470648c1bc929901c57f1c1c65c27a9d2
Esben Dueholm Nørgaard <esben.dueholmnorgaard@musictribe.com>
2023-12-01 08:51:32 +0100

    Day1 Part2 solved.

--- Day01/day01.py
+++ Day01/day01.py
@@ -10,23 +10,50 @@ with open(absFilePath) as inputFile:
 
 inputList = inputStr.splitlines()
 
-# Part 1
-calibrationValueList = []
+# Part 1 & 2
+digitStringList = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+calibrationDigitValueList = []
+digitPositionList = []
+calibrationDigitStringValueList = []
+digitStringPositionList = []
 
 for line in inputList:
+    # Part 1
     digit1 = None
     digit2 = None
     scanRightCount = 0
     scanLeftCount = len(line) - 1
-    while digit1 is None or digit2 is None:
+    while (digit1 is None or digit2 is None) and scanRightCount < len(line):
         if line[scanRightCount].isdigit() and digit1 is None:
             digit1 = line[scanRightCount]
+            digPos1 = scanRightCount
         if line[scanLeftCount].isdigit() and digit2 is None:
             digit2 = line[scanLeftCount]
+            digPos2 = scanLeftCount
         scanRightCount += 1
         scanLeftCount -= 1
 
-    calibrationValue = int(digit1 + digit2)
-    calibrationValueList.append(calibrationValue)
+    calibrationDigitValue = int(digit1 + digit2)
+    calibrationDigitValueList.append(calibrationDigitValue)
+    digitPositionList.append([digPos1, digPos2])
 
-print(sum(calibrationValueList))
+    # Part 2
+    lastIndex = -1
+    firstIndex = -1
+    for digInt, digitString in enumerate(digitStringList, start=1):
+        tmpFirstIdx = line.find(digitString)
+        tmpLastIdx = line.rfind(digitString)
+        if tmpFirstIdx != -1 and tmpFirstIdx < digPos1:
+            digPos1 = tmpFirstIdx
+            digit1 = str(digInt)
+        if tmpLastIdx != -1 and tmpLastIdx > digPos2:
+            digPos2 = tmpLastIdx
+            digit2 = str(digInt)
+
+    calibrationDigitStringValue = int(digit1 + digit2)
+    calibrationDigitStringValueList.append(calibrationDigitStringValue)
+    digitStringPositionList.append([digPos1, digPos2])
+
+
+print('Part 1 answer: ' + str(sum(calibrationDigitValueList)))
+print('Part 2 answer: ' + str(sum(calibrationDigitStringValueList)))

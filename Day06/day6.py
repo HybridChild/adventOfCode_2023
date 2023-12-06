9f1a952279c999a67bae338fccdda343c6d5ca81
Esben Dueholm Nørgaard <esben.dueholmnorgaard@musictribe.com>
2023-12-06 08:11:56 +0100

    Solved Day6 Part1.

--- Day06/day6.py
+++ Day06/day6.py
@@ -1,5 +1,6 @@
 import os
 import re
+import math
 
 def find_numbers(input_string):
     # Define a regular expression pattern to match numbers
@@ -29,5 +30,26 @@ timeList = find_numbers(inputList[0])
 distList = find_numbers(inputList[1])
 gameList = []
 for i in range(len(timeList)):
-    gameList.append({'time': timeList[i], 'dist': distList[i]})
+    gameList.append({'max time': timeList[i], 'current record': distList[i]})
 
+# Part 1
+# Distance = speed * time
+# speed = time of start
+# time = max time - time of start
+margin_mult = 1
+for i, game in enumerate(gameList):
+    best_start_time = math.ceil(game['max time'] / 2)
+    iter_start_time = best_start_time
+    done = False
+    while not done:
+        dist = iter_start_time * (game['max time'] - iter_start_time)
+        if dist <= game['current record']:
+            done = True
+            margin = (iter_start_time - best_start_time) * 2
+            if game['max time'] % 2 == 0:
+                margin -= 1
+            gameList[i]['margin'] = margin
+        iter_start_time += 1
+    margin_mult *= margin
+
+print('Part 1: ' + str(margin_mult))

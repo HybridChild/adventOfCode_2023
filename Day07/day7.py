2b8ac3c22d410e663652f135177ba8580b13b35e
HybridChild <esbendn@tutanota.com>
2023-12-07 18:38:47 +0100

    Add solution for Day7 Part1.

--- Day07/day7.py
+++ Day07/day7.py
@@ -27,6 +27,30 @@ class HandType(Enum):
             return self.value != other.value
         return NotImplemented
 
+
+def handSameTypeGreaterThan(hand1, hand2):
+    for i, card in enumerate(hand1['hand']):
+        if hand1['hand'][i] != hand2['hand'][i]:
+            return hand1['hand'][i] > hand2['hand'][i]
+
+
+def selectionSort(array):
+   # Step through each element of the array
+   for startIdx, elem in enumerate(array):
+        #  smallestIndex is the index of the smallest element we've encountered so far.
+        smallestIdx = startIdx
+
+        # Look for smallest element remaining in the array (starting at startIndex+1)
+        for currentIdx in range(startIdx + 1, len(array)):
+            # If the current element is smaller than our previously found smallest
+            if handSameTypeGreaterThan(array[smallestIdx], array[currentIdx]): # COMPARISON DONE HERE
+                # This is the new smallest number for this iteration
+                smallestIdx = currentIdx
+
+        # Swap our start element with our smallest element
+        array[startIdx], array[smallestIdx] = array[smallestIdx], array[startIdx]
+
+
 def determineHandType(hand):
     counterDict = {}
     for card in hand:
@@ -52,7 +76,7 @@ def determineHandType(hand):
 
 # --- Start here ---
 scriptDir = os.path.dirname(__file__)
-relFilePath = 'example.txt'
+relFilePath = 'input.txt'
 absFilePath = os.path.join(scriptDir, relFilePath)
 
 with open(absFilePath) as inputFile:
@@ -64,6 +88,30 @@ inputList = inputStr.splitlines()
 handList = []
 for line in inputList:
     line = line.split(' ')
-    draw = {'bid': int(line[1]), 'hand': line[0]}
-    draw['type'] = determineHandType(draw['hand'])
-    handList.append(draw)
+
+    line[0] = ' '.join(line[0])
+    line[0] = line[0].replace('T', '10')
+    line[0] = line[0].replace('J', '11')
+    line[0] = line[0].replace('Q', '12')
+    line[0] = line[0].replace('K', '13')
+    line[0] = line[0].replace('A', '14')
+    line[0] = line[0].split(' ')
+    line[0] = [int(num_str) for num_str in line[0]]
+
+    hand = {'hand': line[0], 'bid': int(line[1])}
+    hand['type'] = determineHandType(hand['hand'])
+    handList.append(hand)
+
+# sort draws
+sortedHandList = []
+for type in HandType.__members__.values():
+    typeDrawList = [hand for hand in handList if hand.get('type') == type]
+    selectionSort(typeDrawList)
+    sortedHandList = sortedHandList + typeDrawList
+
+totalWinnings = 0
+for i, hand in enumerate(sortedHandList):
+    sortedHandList[i]['rank'] = i + 1
+    totalWinnings += sortedHandList[i]['rank'] * sortedHandList[i]['bid']
+
+print('Part 1: ' + str(totalWinnings))

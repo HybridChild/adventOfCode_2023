026235bda1dca52d07f9ce6cfb42c3550a2a7cbb
HybridChild <esbendn@tutanota.com>
2023-12-09 09:18:58 +0100

    Solved Day9 Part2.

--- Day09/day9.py
+++ Day09/day9.py
@@ -29,7 +29,7 @@ OASIS_report = []
 for line in inputList:
     OASIS_report.append(find_numbers(line))
 
-# Part 1
+# Expand data
 analysisCollection = []
 for line in OASIS_report:
     diffLineList = [line]
@@ -43,11 +43,22 @@ for line in OASIS_report:
             done = True
     analysisCollection.append(diffLineList)
 
-historyValueSum = 0
+# Calculate history
+futureValueSum = 0
+pastValueSum = 0
 for dataSet in analysisCollection:
+    # Part 1
     dataSet[len(dataSet)-1].append(0)
     for i in range(len(dataSet)-2, -1, -1):
         dataSet[i].append(dataSet[i][len(dataSet[i])-1] + dataSet[i+1][len(dataSet[i+1])-1])
-    historyValueSum += dataSet[0][len(dataSet[0])-1]
+    futureValueSum += dataSet[0][len(dataSet[0])-1]
 
-print('Part 1: ' + str(historyValueSum))
+    # Part 2
+    dataSet[len(dataSet)-1] = [0] + dataSet[len(dataSet)-1]
+    for i in range(len(dataSet)-2, -1, -1):
+        dataSet[i] = [dataSet[i][0] - dataSet[i+1][0]] + dataSet[i]
+    pastValueSum += dataSet[0][0]
+
+
+print('Part 1: ' + str(futureValueSum))
+print('Part 2: ' + str(pastValueSum))

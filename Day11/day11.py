b35787526887e2319dc17b14a1ca37f25c374439
HybridChild <esbendn@tutanota.com>
2023-12-11 16:39:59 +0100

    Solved Day11 Part1

--- Day11/day11.py
+++ Day11/day11.py
@@ -2,7 +2,7 @@ import os
 
 # --- Start here ---
 scriptDir = os.path.dirname(__file__)
-relFilePath = 'example.txt'
+relFilePath = 'input.txt'
 absFilePath = os.path.join(scriptDir, relFilePath)
 
 with open(absFilePath) as inputFile:
@@ -18,54 +18,30 @@ for i in range(len(starMap)):
     for j in range(len(starMap[i])):
         if starMap[i][j] == '#':
             galaxyCounter += 1
-            galaxyDict.append({'position': [i, j], 'id': galaxyCounter})
+            galaxyDict.append({'id': galaxyCounter, 'prePos': (i, j), 'postPos': [i, j]})
 
 # Expand universe
 for i in range(len(starMap)):
     if '#' not in starMap[i]:
         for galaxy in galaxyDict:
-            if galaxy['position'][0] > i:
-                galaxy['position'][0] += 1
+            if galaxy['prePos'][0] > i:
+                galaxy['postPos'][0] += 1
 for i in range(len(starMap[0])):
     column = [row[i] for row in starMap]
     if '#' not in column:
         for galaxy in galaxyDict:
-            if galaxy['position'][1] > i:
-                galaxy['position'][1] += 1
+            if galaxy['prePos'][1] > i:
+                galaxy['postPos'][1] += 1
 
 # Find distances
 distanceSum = 0
 distList = []
 for i in range(len(galaxyDict)-1):
     for j in range(i+1, len(galaxyDict)):
-        pos1 = galaxyDict[i]['position']
-        pos2 = galaxyDict[j]['position']
+        pos1 = galaxyDict[i]['postPos']
+        pos2 = galaxyDict[j]['postPos']
         dist = abs(pos2[0]-pos1[0]) + abs(pos2[1]-pos1[1])
+        distanceSum += dist
         distList.append({'pair': [galaxyDict[i]['id'], galaxyDict[j]['id']], 'dist': dist})
 
 print('Part 1: ' + str(distanceSum))
-
-# ....1........
-# .........2...
-# 3............
-# .............
-# .............
-# ........4....
-# .5...........
-# ............6
-# .............
-# .............
-# .........7...
-# 8....9.......
-
-# 1: [ 0, 4]
-# 7: [10, 9]
-# Between galaxy 1 and galaxy 7: 15
-
-# 3: [2,  0]
-# 6: [7, 12]
-# Between galaxy 3 and galaxy 6: 17
-
-# 8: [11, 0]
-# 9: [11, 5]
-# Between galaxy 8 and galaxy 9: 5

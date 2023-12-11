d39e336b68768ce30acdfccdc9fd868037db30f2
HybridChild <esbendn@tutanota.com>
2023-12-11 18:01:18 +0100

    First attempt at Day8 Part2 - Too slow.

--- Day08/day8.py
+++ Day08/day8.py
@@ -6,9 +6,7 @@ relFilePath = 'input.txt'
 absFilePath = os.path.join(scriptDir, relFilePath)
 
 with open(absFilePath) as inputFile:
-    inputStr = inputFile.read()
-
-inputList = inputStr.splitlines()
+    inputList = inputFile.read().splitlines()
 
 # Parse input
 leftRightSequence = list(inputList[0])
@@ -16,18 +14,21 @@ networkDict = {}
 for i in range(2, len(inputList)):
     networkDict[inputList[i][0:3]] = {'left': inputList[i][7:10], 'right': inputList[i][12:15]}
 
+def navigateNetwork(networkDict, nodePosition, dir):
+    if dir == 'L':
+        nodePosition = networkDict[nodePosition]['left']
+    elif dir == 'R':
+        nodePosition = networkDict[nodePosition]['right']
+    return nodePosition
 
 # Part 1
-done = False
 nodePosition = 'AAA'
 stepCounter = 0
 seqIdx = 0
+done = False
 while not done:
     stepCounter += 1
-    if leftRightSequence[seqIdx] == 'L':
-        nodePosition = networkDict[nodePosition]['left']
-    elif leftRightSequence[seqIdx] == 'R':
-        nodePosition = networkDict[nodePosition]['right']
+    nodePosition = navigateNetwork(networkDict, nodePosition, leftRightSequence[seqIdx])
     if nodePosition == 'ZZZ':
         done = True
     seqIdx += 1
@@ -35,3 +36,25 @@ while not done:
         seqIdx = 0
 
 print('Part 1: ' + str(stepCounter))
+
+# Part 2
+nodeList = []
+for node in networkDict.keys():
+    if node[2] == 'A':
+        nodeList.append(node)
+
+stepCounter = -1
+seqIdx = 0
+done = False
+while not done:
+    stepCounter += 1
+    done = True
+    for i, node in enumerate(nodeList):
+        nodeList[i] = navigateNetwork(networkDict, node, leftRightSequence[seqIdx])
+        if node[2] != 'Z':
+            done = False
+    seqIdx += 1
+    if seqIdx >= len(leftRightSequence):
+        seqIdx = 0
+
+print('Part 2: ' + str(stepCounter))

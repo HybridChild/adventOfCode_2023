31c871da939e3a3320e0442effa48d2dde2a80c7
HybridChild <esbendn@tutanota.com>
2023-12-11 17:13:35 +0100

    Solved Day11 Part2.

--- Day11/day11.py
+++ Day11/day11.py
@@ -1,5 +1,19 @@
 import os
 
+def expandUniverse(galaxyDict, starMap, rate):
+    for i in range(len(starMap)):
+        if '#' not in starMap[i]:
+            for galaxy in galaxyDict:
+                if galaxy['prePos'][0] > i:
+                    galaxy['postPos'][0] += rate - 1
+    for i in range(len(starMap[0])):
+        column = [row[i] for row in starMap]
+        if '#' not in column:
+            for galaxy in galaxyDict:
+                if galaxy['prePos'][1] > i:
+                    galaxy['postPos'][1] += rate - 1
+
+
 # --- Start here ---
 scriptDir = os.path.dirname(__file__)
 relFilePath = 'input.txt'
@@ -9,7 +23,7 @@ with open(absFilePath) as inputFile:
     starMap = inputFile.read().splitlines()
 
 for i in range(len(starMap)):
-    starMap[i] = list(starMap[i])
+    starMap[i] = tuple(starMap[i])
 
 # Find galaxy positions
 galaxyDict = []
@@ -20,28 +34,36 @@ for i in range(len(starMap)):
             galaxyCounter += 1
             galaxyDict.append({'id': galaxyCounter, 'prePos': (i, j), 'postPos': [i, j]})
 
+
 # Expand universe
-for i in range(len(starMap)):
-    if '#' not in starMap[i]:
-        for galaxy in galaxyDict:
-            if galaxy['prePos'][0] > i:
-                galaxy['postPos'][0] += 1
-for i in range(len(starMap[0])):
-    column = [row[i] for row in starMap]
-    if '#' not in column:
-        for galaxy in galaxyDict:
-            if galaxy['prePos'][1] > i:
-                galaxy['postPos'][1] += 1
+expandUniverse(galaxyDict, starMap, 2)
 
 # Find distances
 distanceSum = 0
-distList = []
 for i in range(len(galaxyDict)-1):
     for j in range(i+1, len(galaxyDict)):
         pos1 = galaxyDict[i]['postPos']
         pos2 = galaxyDict[j]['postPos']
         dist = abs(pos2[0]-pos1[0]) + abs(pos2[1]-pos1[1])
         distanceSum += dist
-        distList.append({'pair': [galaxyDict[i]['id'], galaxyDict[j]['id']], 'dist': dist})
 
 print('Part 1: ' + str(distanceSum))
+
+# Reset post positions
+for galaxy in galaxyDict:
+    galaxy['postPos'][0] = galaxy['prePos'][0]
+    galaxy['postPos'][1] = galaxy['prePos'][1]
+
+# Run expansion again with new rate
+expandUniverse(galaxyDict, starMap, 1000000)
+
+# Calculate new distances
+distanceSum = 0
+for i in range(len(galaxyDict)-1):
+    for j in range(i+1, len(galaxyDict)):
+        pos1 = galaxyDict[i]['postPos']
+        pos2 = galaxyDict[j]['postPos']
+        dist = abs(pos2[0]-pos1[0]) + abs(pos2[1]-pos1[1])
+        distanceSum += dist
+
+print('Part 2: ' + str(distanceSum))

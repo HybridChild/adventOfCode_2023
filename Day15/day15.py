c0b8c8aa521a694e21dbe8d02c9feb93575e9728
HybridChild <esbendn@tutanota.com>
2023-12-16 12:00:48 +0100

    Add solution for Day15 Part2

--- Day15/day15.py
+++ Day15/day15.py
@@ -9,6 +9,33 @@ def runHASH(strIn):
         currentValue %= 256
     return currentValue
 
+def insertLens(boxDict, lens):
+    if lens['label'] not in boxDict[lens['box']].keys():
+        boxDict[lens['box']][lens['label']] = {}
+        boxDict[lens['box']][lens['label']]['position'] = boxDict[lens['box']]['lensCnt'] + 1
+        boxDict[lens['box']]['lensCnt'] += 1
+    boxDict[lens['box']][lens['label']]['focal'] = lens['focal']
+
+def removeLens(boxDict, lens):
+    if lens['label'] in boxDict[lens['box']].keys():
+        remLens = boxDict[lens['box']].pop(lens['label'])
+        boxDict[lens['box']]['lensCnt'] -= 1
+        for key in boxDict[lens['box']].keys():
+            if key != 'lensCnt':
+                if boxDict[lens['box']][key]['position'] > remLens['position']:
+                    boxDict[lens['box']][key]['position'] -= 1
+
+def calculateFocusingPower(boxIdx, lensDict):
+    focusPowerSum = 0
+    for key in lensDict:
+        if key != 'lensCnt':
+            focusPower = 1
+            focusPower *= 1 + boxIdx
+            focusPower *= lensDict[key]['position']
+            focusPower *= lensDict[key]['focal']
+            focusPowerSum += focusPower
+    return focusPowerSum
+
 
 # --- Start here ---
 scriptDir = os.path.dirname(__file__)
@@ -24,3 +51,31 @@ for step in initSeqList:
     valueSum += runHASH(step)
 
 print('Part 1: ' + str(valueSum))
+
+# Part 2
+for i, step in enumerate(initSeqList):
+    stepDict = {}
+    actIdx = step.find('-')
+    if actIdx == -1:
+        actIdx = step.find('=')
+        focal = int(step[actIdx+1:])
+        stepDict['focal'] = focal
+    stepDict['label'] = step[0:actIdx]
+    stepDict['box'] = runHASH(stepDict['label'])
+    initSeqList[i] = stepDict
+
+boxDict = {}
+for i in range(256):
+    boxDict[i] = {'lensCnt': 0}
+
+for step in initSeqList:
+    if 'focal' in step.keys():
+        insertLens(boxDict, step)
+    else:
+        removeLens(boxDict, step)
+
+focusPowerSum = 0
+for box in boxDict:
+    focusPowerSum += calculateFocusingPower(box, boxDict[box])
+
+print('Part 2: ' + str(focusPowerSum))

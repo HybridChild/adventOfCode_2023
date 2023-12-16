e261b0e4adbf508d41125f1ab9bea59b9e129d6d
HybridChild <esbendn@tutanota.com>
2023-12-16 22:31:02 +0100

    Solved Day16 Part2

--- Day16/day16.py
+++ Day16/day16.py
@@ -1,4 +1,6 @@
 import os
+import copy
+import time
 
 def lightBeam(mirrorMap, coord, enterFrom):
     if enterFrom in mirrorMap[coord[0]][coord[1]]['enteredFrom']:
@@ -58,8 +60,24 @@ def lightBeam(mirrorMap, coord, enterFrom):
 
     return nextCoords
 
+def shineLightBeam(mirrorMap, startCoord):
+    coordList = [startCoord]
+    while len(coordList) > 0:
+        nextCoord = coordList.pop()
+        coordList.extend(lightBeam(mirrorMap, nextCoord['coord'], nextCoord['enterFrom']))
+
+def countEnergized(mirrorMap):
+    energizedTileCount = 0
+    for row in range(len(mirrorMap)):
+        for col in range(len(mirrorMap[0])):
+            if len(mirrorMap[row][col]['enteredFrom']) > 0:
+                energizedTileCount += 1
+    return energizedTileCount
+
 
 # --- Start here ---
+start_time = time.time()
+
 scriptDir = os.path.dirname(__file__)
 relFilePath = 'input.txt'
 absFilePath = os.path.join(scriptDir, relFilePath)
@@ -67,22 +85,35 @@ absFilePath = os.path.join(scriptDir, relFilePath)
 with open(absFilePath) as inputFile:
     mirrorMap = [list(row) for row in inputFile.read().splitlines()]
 
-
 for row in range(len(mirrorMap)):
     for col in range(len(mirrorMap[0])):
         mirrorMap[row][col] = {'coordinate': (row, col), \
                                'symbol': mirrorMap[row][col], \
                                'enteredFrom': []}
 
-coordList = [{'coord': (0, 0), 'enterFrom': 'left'}]
-while len(coordList) > 0:
-    nextCoord = coordList.pop()
-    coordList.extend(lightBeam(mirrorMap, nextCoord['coord'], nextCoord['enterFrom']))
-
-energizedTileCount = 0
+coordList = []
 for row in range(len(mirrorMap)):
-    for col in range(len(mirrorMap[0])):
-        if len(mirrorMap[row][col]['enteredFrom']) > 0:
-            energizedTileCount += 1
+    coordList.append({'coord': (row, 0), 'enterFrom': 'left'})
+    coordList.append({'coord': (row, len(mirrorMap[row]) - 1), 'enterFrom': 'right'})
 
-print('Part 1: ' + str(energizedTileCount))
+for col in range(len(mirrorMap[0])):
+    coordList.append({'coord': (0, col), 'enterFrom': 'top'})
+    coordList.append({'coord': (len(mirrorMap) - 1, col), 'enterFrom': 'bottom'})
+
+energizedDict = {}
+for startCoord in coordList:
+    mirrorMapCopy = copy.deepcopy(mirrorMap)
+    shineLightBeam(mirrorMapCopy, startCoord)
+    energizedDict[str(startCoord['coord']) + startCoord['enterFrom']] = countEnergized(mirrorMapCopy)
+
+maxEnergized = 0
+for key in energizedDict.keys():
+    if energizedDict[key] > maxEnergized:
+        maxEnergized = energizedDict[key]
+
+print('Part 1: ' + str(energizedDict['(0, 0)left']))
+print('Part 2: ' + str(maxEnergized))
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Elapsed time: {elapsed_time} seconds")
